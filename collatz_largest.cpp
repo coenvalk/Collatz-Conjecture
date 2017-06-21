3bfdef1c8fad68ad33be73120025a47ef20f0e87
coenvalk <clcjvalk@gmail.com>
2017-06-21 17:17:25 -0400

    adding collatz largest optimizations

--- collatz_largest.cpp
+++ collatz_largest.cpp
@@ -1,23 +1,32 @@
 #include <iostream>
+#include <map>
 
 #include "BigInt/BigIntegerLibrary.hh"
 
-BigUnsigned collatz(BigUnsigned n) {
+BigUnsigned collatz(BigUnsigned n, std::map<BigUnsigned, BigUnsigned>& cache) {
   BigUnsigned i = 0;
+  std::map<BigUnsigned, BigUnsigned>::iterator it;
   while (n != 1) {
-    if (n % 2 == 1) {
-      n *= 3;
-      n += 1;
+    it = cache.find(n);
+    if (it == cache.end()) {
+      if (n % 2 == 1) {
+	n *= 3;
+	n += 1;
+      } else {
+	n /= 2;
+      }
+      i++;
     } else {
-      n /= 2;
+      i += it->second;
     }
-    i++;
   }
+  cache[n] = i;
   return i;
 }
 
 int main(int argc, char* argv[]) {
   BigUnsigned N = 100;
+  std::map<BigUnsigned, BigUnsigned> cache;
   if (argc > 1) {
     std::string A(argv[1]);
     N = stringToBigUnsigned(A);
@@ -25,7 +34,7 @@ int main(int argc, char* argv[]) {
   BigUnsigned largest = 1;
   BigUnsigned largest_collatz = 0;
   for (BigUnsigned i = 1; i < N; i++) {
-    BigUnsigned tmp = collatz(i);
+    BigUnsigned tmp = collatz(i, cache);
     if (tmp > largest_collatz) {
       largest_collatz = tmp;
       largest = i;
